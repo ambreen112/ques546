e1a1a1a69a58d177d89c02d7834bf9382117ba4b
ambreen112 <160263381+ambreen112@users.noreply.github.com>
2024-03-05 22:17:27 +0500

    Add files via upload

---
+++ ques546.js
@@ -0,0 +1,9 @@
+//Question No # 5
+var famous_person = "Hazrat Ali";
+var message = "Calmness is a sign of intelligence";
+console.log(famous_person, message);
+//Question No # 6
+var withwhitespace = "\n\t Mahanoor\t\n";
+console.log(withwhitespace);
+var withoutspace = "Mahanoor";
+console.log(withoutspace);
